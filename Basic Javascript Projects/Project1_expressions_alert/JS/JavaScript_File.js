f3fe9bcc16ac058e5bc59e613925d89e6182deb9
marconelele <jimmy187bob@Gmail.com>
2023-04-21 09:53:21 +0100

    assignment

---
+++ Basic Javascript Projects/Project1_expressions_alert/JS/JavaScript_File.js
@@ -0,0 +1,8 @@
+var A = "This is a string called \"A\"";
+var B = "Concatenated" + " String";
+var Family = "The Arezzinis", Dad = "Jeremiah", Mom = "Hermoine", Daughter = "Penny", Son = "Zorro";
+
+function My_First_Function() {                                              // Defining function and naming it
+    var str = "This is the button text!";                                   // Defining a variable and giving it a string value
+    document.getElementById("Button_Text").innerHTML = str;                 // Putting the value of the variable into the HTML elementFromPoint with the button text id
+}
---
+++ Basic Javascript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,4 @@
+var Sent1 = "This is the beginning of a string";
+var Sent2 = " and this is the end of a string";
+
+document.write(Sent1 + Sent2);
