0e5c7768101790c8240109863b205dc085c258c6
marconelele <jimmy187bob@Gmail.com>
2023-04-21 10:27:56 +0100

    project 2 assignment

---
+++ Basic Javascript Projects/Project2_functions/js/main.js
@@ -0,0 +1,10 @@
+function buttonPress() {
+    var str = "Surprise!";
+    document.getElementById("Button_Text").innerHTML = str;
+}
+
+function myFunction() {
+    var text1 = "I am learning";
+    text1 += " a lot from this course!";
+    document.getElementById("Concatenate").innerHTML = text1;
+}
